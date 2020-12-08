6fff0315f0d1b45b66c04ac81d7ecc03df9731a2
Prathamesh-Shanbhag <70568620+Prathamesh-Shanbhag@users.noreply.github.com>
2020-12-09 00:00:12 +0530

    Diet Section Pilot Work

--- js/main.js
+++ js/main.js
@@ -167,3 +167,18 @@ date.innerHTML = new Date().getFullYear();
 //     linksContainer.style.height = 0;
 //   });
 // });
+
+// Diet Section>>>>>>>>>>>
+const imgBx = document.querySelector('.imgBx');
+const slides = imgBx.getElementsByTagName('img');
+let i = 0;
+function nextSlide() {
+  slides[i].classList.remove('active');
+  i = (i + 1) % slides.length;
+  slides[i].classList.add('active');
+}
+function prevSlide() {
+  slides[i].classList.remove('active');
+  i = (i - 1 + slides.length) % slides.length;
+  slides[i].classList.add('active');
+}
